da868437f9c9c576e9edec94f688bcd20655dd91
mboerwinkle <mboerwinkle@gmail.com>
2014-05-31 00:53:54 +0000

    add mkship.c

---
+++ server/mkship.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "globals.h"
+int mkship(char name[MAXNAMELEN]){
+	char path[MAXNAMELEN+6];
+	FILE *fp;
+	sprintf(path, "ships/%s", name);
+	if((fp = fopen(path, "r")) != NULL){
+		fclose(fp);
+		return(-1);
+	}
+	fp = fopen(path, "w");
+	fprintf(fp, "0_0\n7000 7000");
+	fclose(fp);
+	return(0);
+}
