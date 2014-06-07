0a7f78311f1df31e0d21e5c176a00ee250c619fa
Simon Boerwinkle <sboerwinkle@gmail.com>
2014-06-07 21:07:43 +0000

    Possibly finished the rudimentary server side...

--- server/networking.c
+++ server/networking.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <limits.h>
 #include "globals.h"
 
 #include <arpa/inet.h>
@@ -15,9 +17,33 @@ typedef struct client{
 
 static client* clientList = NULL;
 
+static int sockfd;
+
+void sendInfo(){
+	struct sockaddr_in sendAddr = {.sin_family=AF_INET, .sin_port=htons(3334)};
+	int16_t data[3*100];
+	int dataLen;
+	client* conductor = clientList;
+	while(conductor){
+		sector *sec = conductor->myShip->mySector;
+		entity *runner = sec->firstentity;
+		dataLen = 0;
+		while(runner){
+			data[dataLen+0] = 0x01*runner->theta+0x10*0/*flame or not*/+0x20*0/*faction*/+0x80*0/*ship type*/;
+			data[dataLen+1] = displacementX(conductor->myShip, runner)/64;
+			data[dataLen+2] = displacementY(conductor->myShip, runner)/64;
+			dataLen+=3;
+			runner = runner->next;
+		}
+		sendAddr.sin_addr.s_addr = conductor->addr.sin_addr.s_addr;
+		sendto(sockfd, (char*)data, dataLen*2, 0, (struct sockaddr*)&sendAddr, sizeof(sendAddr));
+		conductor = conductor->next;
+	}
+}
+
 void* netListen(void* whoGivesADern){
 	puts("So, yeah, threading.");
-	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 	if(sockfd < 0){
 		puts("When in danger,\nOr in doubt,\nRun in circles!\nScream and shout!");
 		return NULL;
