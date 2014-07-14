fb7a50c41ad4899258eeeb8311a64f22aae652f3
Simon Boerwinkle <sboerwinkle@gmail.com>
2014-07-14 17:22:17 +0000

    OMG no

---
+++ client/trails.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+#include "trails.h"
+
+static trail* trails = NULL;
+static int numTrails = 0, maxTrails = 0;
+
+void addTrail(int x1, int y1, int x2, int y2, int type){
+	if(numTrails == maxTrails){
+		trails = realloc(trails, sizeof(trail)*(maxTrails+=2));
+	}
+	trails[numTrails].x1 = x1;
+	trails[numTrails].y1 = y1;
+	trails[numTrails].x2 = x2;
+	trails[numTrails].y2 = y2;
+	trails[numTrails].type = type;
+	trails[numTrails].life = 20;
+	numTrails++;
+}
+
+void drawTrails(SDL_Renderer* render){
+	int i = 0;
+	for(; i < numTrails; i++){
+		if(trails[i].type == 0){
+			SDL_SetRenderDrawColor(render, 255, 0, 0, 12*trails[i].life);
+			SDL_RenderDrawLine(render, trails[i].x1, trails[i].y1, trails[i].x2, trails[i].y2);
+		}
+		if(--trails[i].life == 0){
+			trails[i] = trails[--numTrails];
+			i--;
+		}
+	}
+}
